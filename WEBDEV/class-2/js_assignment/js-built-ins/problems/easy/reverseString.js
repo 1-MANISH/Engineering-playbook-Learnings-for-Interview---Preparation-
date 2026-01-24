324034a118a37342e2ca179208b1bed63060f50d
manish_patidar <shanimpatidar@gmail.com>
2026-01-24 22:36:38 +0530

    Solving interview+assignment quesiton for js foundation

---
+++ WEBDEV/class-2/js_assignment/js-built-ins/problems/easy/reverseString.js
@@ -0,0 +1,30 @@
+/*
+  Write a function `reve` which takes a string as input and returns the reversed version of the string.
+
+  What is reversing a string?
+  - Reversing a string means rearranging its characters in the opposite order.
+
+  Example:
+  - Input: "Sumana"
+  - Output: "anamuS"
+
+  - Input: "hello"
+  - Output: "olleh"
+
+  - Input: ""
+  - Output: ""
+
+  Once you've implemented the logic, test your code by running
+  - `npm run test-reverseString`
+*/
+
+function reverseString(str) {
+        let newStr = '';
+        for(let i = str.length - 1; i >= 0; i--) {
+            newStr += str[i];
+        }
+        str = newStr;
+        return str;
+}
+
+module.exports = reverseString;
