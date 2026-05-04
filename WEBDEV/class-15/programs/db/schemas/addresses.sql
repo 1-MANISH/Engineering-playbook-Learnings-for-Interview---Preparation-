67c57225c2e116ef62376a7ea3640fcdff8b491a
manish_patidar <shanimpatidar@gmail.com>
2026-05-04 13:48:16 +0530

    updated with sql releation + joins unserstanding + practise queries

---
+++ WEBDEV/class-15/programs/db/schemas/addresses.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS addresses(
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER NOT NULL,
+        city VARCHAR(255)  NOT NULL,
+        country VARCHAR(255) NOT NULL,
+        street VARCHAR(255) NOT NULL,
+        pincode VARCHAR(20) NOT NULL,
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+)
